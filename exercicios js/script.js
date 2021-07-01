40c28fd1ad298043a5a480b83c6180560fe143c6
Luana Giusto <luanagiusto@gmail.com>
2021-07-01 16:32:23 -0300

    exercicios HTML e alguns de JS

---
+++ exercicios js/script.js
@@ -0,0 +1,49 @@
+// FizzBuzz
+// Divisivel por 3 => 'Fizz'
+// Divisivel por 5 => 'Buzz'
+// Divisivel por 3 e 5 => 'FizzBuzz'
+// Se não for um número => 'Não é um número'
+// Se não for visível nem por 3 e nem por 5 => Entrada
+
+let resultado = fizzBuzz();
+console.log(resultado)
+
+function fizzBuzz(entrada) {
+    if (typeof entrada !== 'number')
+        return 'Não é um número';
+    if ((entrada % 3 === 0) && (entrada % 5 === 0))
+        return 'FizzBuzz';
+    if (entrada % 3 === 0)
+        return 'Fizz';
+    if (entrada % 5 === 0)
+        return 'Buzz';
+
+    return entrada;
+}
+
+
+// Reverse a string
+
+let newStr = '';
+
+function reverseAString(str) {
+    for (let i = str.length - 1; i >= 0; i--)
+        newStr += str[i];
+}
+console.log(newStr);
+
+//let resultado = reverseAString('Hello')
+
+
+
+
+// Conversor de temperatura - Celsius para Fahrenheit
+
+function convertToFahrenheit(value) {
+    return value * 1.8 + 32
+}
+
+let result = convertToFahrenheit()
+console.log(`O valor em Fahrenheit é ${result}`)
+
+
---
+++ exercicios js/todolist.js
@@ -0,0 +1,38 @@
+// To Do List
+
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+let lists = []
+
+newListForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName === null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
+
+
+function createList(name) {
+    return {id: Date.now().toString(), name: name }
+}
+
+function render() {
+    clearElement(listContainer) //vai limpar toda vez que atualizar a pagina
+    lists.forEach(function(list) {
+        const item = document.createElement('li')
+        item.classList.add('item')
+        item.innerText = list.name
+        listContainer.appendChild(item)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
